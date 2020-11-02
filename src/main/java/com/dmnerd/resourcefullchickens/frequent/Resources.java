1c3a556801fc88f7c8a417310b674751d51263ef
Adam <7889445+DMNerd@users.noreply.github.com>
2020-11-02 20:18:50 +0100

    Implementing Registrate

---
+++ src/main/java/com/dmnerd/resourcefullchickens/frequent/Resources.java
@@ -0,0 +1,8 @@
+package com.dmnerd.resourcefullchickens.frequent;
+
+public class Resources {
+    public static final String MODID = "resourcefullchickens";
+    public static final String MODNAME = "ResourceFull Chickens";
+    public static final String VER = "1";
+
+}
---
+++ src/main/java/main/java/com/dmnerd/resourcefullchickens/ResourcefullChickens.java
@@ -0,0 +1,9 @@
+package main.java.com.dmnerd.resourcefullchickens;
+
+import com.dmnerd.resourcefullchickens.frequent.Resources;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Resources.MODID)
+public class ResourcefullChickens {
+
+}
